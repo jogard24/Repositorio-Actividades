a1570d9ef77d70e8870b48e5f273b4bbfd4cfbe4
javid495 <juandavidcacereshernandez@gmail.com>
2026-04-25 16:37:01 -0500

    fetch: Se añadio tanto el archivo.json, verificacion de usuarios, añadir los comentarios ingresados

---
+++ Servidor/helper/fetch.js
@@ -0,0 +1,20 @@
+export const request = async (url) => {
+    const solicitud = await fetch(`http://localhost:3000/${url}`);
+    const data = await solicitud.json();
+    return data;
+};
+
+// export const destroy = async (url) => {
+//     const solicitud = await fetch(`http://localhost:3000/${url}`, {
+//         method: 'DELETE', // Especificamos que queremos borrar
+//         headers: {
+//             'Content-Type': 'application/json'
+//         }
+//     });
+
+//     if (!solicitud.ok) {
+//         throw new Error("No se pudo eliminar el recurso");
+//     }
+
+//     return await solicitud.json();
+// };
