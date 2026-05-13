e46a9a73cc1ab8180e687f3453f48c4f5b073fcb
javid495 <juandavidcacereshernandez@gmail.com>
2026-05-12 21:49:52 -0500

    feat: Se esta aplicando modulazacion a partes del proyecto y se esta averiguando la implemetacion de serve al proyecto

---
+++ Cliente/Peticiones/BuscarNombre.js
@@ -0,0 +1,13 @@
+import { request } from "../helper/index.js";
+
+export const BuscarNombre = async (nombre) =>{
+
+    const user = await request("users");
+        // Se valida que el usuario este en la base de datos
+        const usersExiste = user.find(u => String(u.name) === String(nombre));
+        
+        console.log(usersExiste);
+        
+        //Retornamo un valor booleano
+        return usersExiste ? true : false;
+}
