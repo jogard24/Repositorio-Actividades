c7dbe71ac4d12bb5bb3fa52c4a5646299751d4f5
javid495 <juandavidcacereshernandez@gmail.com>
2026-05-20 16:10:27 -0500

    fix: se modularizo la lista de usuarios y el inconveniente de redenrizado de la lista de usuarios

--- Cliente/VistaAgregar/AgregarControlador.js
+++ Cliente/VistaAgregar/AgregarControlador.js
@@ -1,8 +1,11 @@
 import { agregar } from "./mostrarAgregar.js"
+import { listarUsuario } from "../Peticiones/ListarUsuario.js"
 
 export const agregarControlador = () => {
  
     const editable = document.querySelector('div > div');
-    editable.innerHTML = agregar();
+
+    listarUsuario();
+
     
 }
