d9001a706be90cf4e259071765bba1e9c7c023e4
javid495 <juandavidcacereshernandez@gmail.com>
2026-05-20 17:55:02 -0500

    Fix: Se arreglo el metodo de actualizar en el controlador del enrutador

--- Cliente/Peticiones/index.js
+++ Cliente/Peticiones/index.js
@@ -3,6 +3,8 @@ export * from "./AgregarT.js";
 export * from "./BorrarTarea.js";
 export * from "./BuscarNombre.js";
 export * from "./ObtenerTareasUser.js";
+export * from "./ListarUsuario.js";
+export * from "./MostrarTareas.js"
 
 
 
